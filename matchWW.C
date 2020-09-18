160dc2c6170cc4bb3aa6645764a8c93c2e8a7ac1
PeiranLi001 <69635121+PeiranLi001@users.noreply.github.com>
2020-09-18 14:07:08 +0800

    Update matchWW.C

--- matchWW.C
+++ matchWW.C
@@ -205,10 +205,10 @@ void matchWW(){
     Eta_q.push_back(gen_Subleading_WpJets_Eta);
     Eta_q.push_back(gen_leading_WmJets_Eta);
     Eta_q.push_back(gen_Subleading_WmJets_Eta);
-    Eta_AK4jet.push_back(AK4GEN_AllResolved_onShellJet1_Eta);
-    Eta_AK4jet.push_back(AK4GEN_AllResolved_onShellJet2_Eta);
-    Eta_AK4jet.push_back(AK4GEN_AllResolved_offShellJet1_Eta);
-    Eta_AK4jet.push_back(AK4GEN_AllResolved_offShellJet2_Eta);
+    Eta_AK4jet[0]=(AK4GEN_AllResolved_onShellJet1_Eta);
+    Eta_AK4jet[1]=(AK4GEN_AllResolved_onShellJet2_Eta);
+    Eta_AK4jet[2]=(AK4GEN_AllResolved_offShellJet1_Eta);
+    Eta_AK4jet[3]=(AK4GEN_AllResolved_offShellJet2_Eta);
   
     Phi_q.push_back(gen_leading_WpJets_Eta);
     Phi_q.push_back(gen_Subleading_WpJets_Eta);
